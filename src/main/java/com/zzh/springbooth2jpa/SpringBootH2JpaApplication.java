9b1016136deb73fa6a0d4f0e8a2347f7d13d3b59
gnuzman <94644378@qq.com>
2017-08-29 21:40:45 +0800

    create

---
+++ src/main/java/com/zzh/springbooth2jpa/SpringBootH2JpaApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.springbooth2jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2JpaApplication.class, args);
+	}
+}
